517ab706107dd9dbfad3c431d09250e5105e975a
Marcelo Siqueira Oliveira <mss.siqueira@gmail.com>
2024-02-29 15:42:17 -0300

    aula de média de notas de um aluno sem orientação objeto

---
+++ NotasDoAluno/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NotasDoAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a nota do 1° trimestre:");
+            double nota1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota do 2° trimestre:");
+            double nota2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a nota do 3° trimestre:");
+            double nota3 = double.Parse(Console.ReadLine());
+
+            double media = ((nota1 * 30) + (nota2 * 35) + (nota3 * 35)) / 10;
+
+        
+            if (media >= 60)
+            {
+                Console.WriteLine($"Sua média é: {media:f2} e você está aprovado");
+            }
+            else
+            {
+                Console.WriteLine($"Sua média é: {media:f2} e você está reprovado");
+            }
+        }
+    }
+}
